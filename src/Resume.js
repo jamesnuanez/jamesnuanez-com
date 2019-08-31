3128d6e3ef52e2e7199ed8a399e62b3a78d00cc6
James Nuanez <james@jamesnuanez.com>
2019-08-30 21:26:24 -0700

    Restructure resume page to allow FinalLink below resume

--- src/Resume.js
+++ src/Resume.js
@@ -10,116 +10,127 @@ class Resume extends Component {
   render() {
     return (
       <section className="Resume">
-        <h1>James Nuanez</h1>
-        <p>
-          <a href="mailto:james@jamesnuanez.com">james@jamesnuanez.com</a>
-          {' '}<span className="Resume-divider">|</span>{' '}
-          <a href="https://github.com/jamesnuanez">github.com/jamesnuanez</a>
-          {' '}<span className="Resume-divider">|</span>{' '}
-          <a href="https://www.linkedin.com/in/jamesnuanez">linkedin.com/in/jamesnuanez</a>
-        </p>
-        <hr />
+        <div className="Resume-page">
+          <h1>James Nuanez</h1>
+          <p>
+            <a
+              className="text-link"
+              href="mailto:james@jamesnuanez.com"
+            >james@jamesnuanez.com</a>
 
-        <main className="Resume-main">
-          <div className="Resume-left">
+            {' '}<span className="Resume-divider">|</span>{' '}
 
-            <h2>Skills</h2>
-            <ul>
-              <li>Node.js with Express</li>
-              <li>React</li>
-              <li>MySQL</li>
-              <li>Git / GitHub / GitLab</li>
-              <li>Linux CLI / Bash</li>
-              <li>AWS</li>
-              <li>D3</li>
-              <li>MongoDB</li>
-              <li>FreeSWITCH</li>
-              <li>UI/UX Design</li>
-            </ul>
+            <a
+              className="text-link"
+              href="https://github.com/jamesnuanez"
+            >github.com/jamesnuanez</a>
 
-            <h2>Education</h2>
+            {' '}<span className="Resume-divider">|</span>{' '}
 
-            <h3>Online Courses</h3>
-            <ul>
-              <li>Wes Bos ES6</li>
-              <li>Wes Bos Node</li>
-              <li>Colt Steele React Bootcamp</li>
-              <li>Colt Steele MySQL Bootcamp</li>
-              <li>Colt Steele Web Dev Bootcamp</li>
-            </ul>
+            <a
+              className="text-link"
+              href="https://www.linkedin.com/in/jamesnuanez"
+            >linkedin.com/in/jamesnuanez</a>
+          </p>
+          <hr />
 
-            <h3>BFA in Illustration</h3>
-            <ul>
-              <li>California State University,</li>
-              <li>Long Beach</li>
-              <li>GPA: 3.8, 2007</li>
-            </ul>
+          <div className="Resume-content">
+            <div className="Resume-left">
+              <h2>Skills</h2>
+              <ul>
+                <li>Node.js with Express</li>
+                <li>React</li>
+                <li>MySQL</li>
+                <li>Git / GitHub / GitLab</li>
+                <li>Linux CLI / Bash</li>
+                <li>AWS</li>
+                <li>D3</li>
+                <li>MongoDB</li>
+                <li>FreeSWITCH</li>
+                <li>UI/UX Design</li>
+              </ul>
 
-            <h3>CompTIA Network+</h3>
-            <ul>
-              <li>Exam N10-006</li>
-              <li>July 2017</li>
-            </ul>
+              <h2>Education</h2>
 
-            <h3>CompTIA A+</h3>
-            <ul>
-              <li>Exam 220-901 & 220-902</li>
-              <li>July 2016</li>
-            </ul>
+              <h3>Online Courses</h3>
+              <ul>
+                <li>Wes Bos ES6</li>
+                <li>Wes Bos Node</li>
+                <li>Colt Steele React Bootcamp</li>
+                <li>Colt Steele MySQL Bootcamp</li>
+                <li>Colt Steele Web Dev Bootcamp</li>
+              </ul>
 
-            <h3>SIP School Certified Associate</h3>
-            <ul>
-              <li>The SIP School</li>
-              <li>June 2018</li>
-            </ul>
+              <h3>BFA in Illustration</h3>
+              <ul>
+                <li>California State University,</li>
+                <li>Long Beach</li>
+                <li>GPA: 3.8, 2007</li>
+              </ul>
 
+              <h3>CompTIA Network+</h3>
+              <ul>
+                <li>Exam N10-006</li>
+                <li>July 2017</li>
+              </ul>
+
+              <h3>CompTIA A+</h3>
+              <ul>
+                <li>Exam 220-901 & 220-902</li>
+                <li>July 2016</li>
+              </ul>
+
+              <h3>SIP School Certified Associate</h3>
+              <ul>
+                <li>The SIP School</li>
+                <li>June 2018</li>
+              </ul>
+            </div>
+            <div className="Resume-right">
+              <h2>Experience</h2>
+              
+              <h3>
+                <span className="avoid-wrap">Freelance Full Stack Developer</span>
+                {' '}<span className="avoid-wrap">(2017-Present)</span></h3>
+              <h4>Portfolio at www.jamesnuanez.com</h4>
+              <p>Building full stack, responsive websites using Node.js and Express, with NGINX on Linux, hosted on Digital Ocean. Managed via command line, including system updates and TLS certification management with Let’s Encrypt.</p>
+
+              <h3>
+                <span className="avoid-wrap">VoIP Software Developer</span>
+                {' '}<span className="avoid-wrap">(Jul 2019 - Present)</span></h3>
+              <h4>At Phone.com, a VoIP service provider.</h4>
+              <p>With a team, build a new VoIP call routing service on FreeSWITCH. Implement features and bug fixes in Lua, and connect to a MySQL database. Code managed in GitLab.</p>
+
+              <h3>
+                <span className="avoid-wrap">VoIP Systems Analyst</span>
+                {' '}<span className="avoid-wrap">(Nov 2018 - Jul 2019)</span></h3>
+              <h4>At Phone.com, a VoIP service provider.</h4>
+              <p>Various projects surrounding VoIP KPIs and monitoring, including building two full stack Node.js applications from scratch:</p>
+              <ol>
+                <li><em>Call records search tool:</em> built in AWS, including a dedicated VPC, RDS database, storage in S3, Lambda Functions to process recordings and collect and reconcile API data, and SNS alerts for error handling.</li>
+                <li><em>VoIP Stats tool:</em> collect and display call statistics and alerts, with a local MySQL database, connections to the PagerDuty and Jira APIs, D3 for data visualizations, uptime calculated on-the-fly for any time range, and various Bash scripts for collecting images from other web services.</li>
+              </ol>
+
+              <h3>
+                <span className="avoid-wrap">Software Quality Assurance Specialist</span>
+                {' '}<span className="avoid-wrap">(Feb 2018 - Oct 2018)</span></h3>
+              <h4>At Phone.com, a VoIP service provider.</h4>
+              <p>Tested in-house applications (Android, iOS, macOS, Windows) and VoIP call routing software. Wrote regression tests, documented bugs, and worked with developers to improve user experience.</p>
+
+              <h3>
+                <span className="avoid-wrap">English Teacher in South Korea</span>
+                {' '}<span className="avoid-wrap">(2013-2015)</span></h3>
+              <h4>At Guhak Elementary School in Busan, South Korea.</h4>
+              <p>Taught alongside multiple Korean teachers. Brainstormed and planned lessons in conjunction with the Korean teachers, including normal daily English lessons and multi-week summer camps.</p>
+
+              <h3>
+                <span className="avoid-wrap">Web Design and Bug Tracking</span>
+                {' '}<span className="avoid-wrap">(2008-2009, 2011-2012)</span></h3>
+              <h4>At GreekBill, Inc. and MemberPlanet in Torrance, California.</h4>
+              <p>Redesigned existing website to improve UI/UX, performed QA testing and bug tracking, and wrote functional documentation.</p>
+            </div>
           </div>
-          <div className="Resume-right">
-
-            <h2>Experience</h2>
-            
-            <h3>
-              <span className="avoid-wrap">Freelance Full Stack Developer</span>
-              {' '}<span className="avoid-wrap">(2017-Present)</span></h3>
-            <h4>Portfolio at www.jamesnuanez.com</h4>
-            <p>Building full stack, responsive websites using Node.js and Express, with NGINX on Linux, hosted on Digital Ocean. Managed via command line, including system updates and TLS certification management with Let’s Encrypt.</p>
-
-            <h3>
-              <span className="avoid-wrap">VoIP Software Developer</span>
-              {' '}<span className="avoid-wrap">(Jul 2019 - Present)</span></h3>
-            <h4>At Phone.com, a VoIP service provider.</h4>
-            <p>With a team, build a new VoIP call routing service on FreeSWITCH. Implement features and bug fixes in Lua, and connect to a MySQL database. Code managed in GitLab.</p>
-
-            <h3>
-              <span className="avoid-wrap">VoIP Systems Analyst</span>
-              {' '}<span className="avoid-wrap">(Nov 2018 - Jul 2019)</span></h3>
-            <h4>At Phone.com, a VoIP service provider.</h4>
-            <p>Various projects surrounding VoIP KPIs and monitoring, including building two full stack Node.js applications from scratch:</p>
-            <ol>
-              <li><em>Call records search tool:</em> built on AWS, including a dedicated VPC, RDS database, call recordings in S3, Lambda Functions to process recordings and collect and reconcile API data, and SNS alerts for error handling.</li>
-              <li><em>VoIP Stats tool:</em> collect and display call statistics and alerts, with a local MySQL database, connections to the PagerDuty and Jira APIs, D3 for data visualizations, uptime calculated on-the-fly for any time range, and various Bash scripts for collecting images from other web services.</li>
-            </ol>
-
-            <h3>
-              <span className="avoid-wrap">Software Quality Assurance Specialist</span>
-              {' '}<span className="avoid-wrap">(Feb 2018 - Oct 2018)</span></h3>
-            <h4>At Phone.com, a VoIP service provider.</h4>
-            <p>Tested in-house applications (Android, iOS, macOS, Windows) and VoIP call routing software. Wrote regression tests, documented bugs, and worked with developers to improve user experience.</p>
-
-            <h3>
-              <span className="avoid-wrap">English Teacher in South Korea</span>
-              {' '}<span className="avoid-wrap">(2013-2015)</span></h3>
-            <h4>At Guhak Elementary School in Busan, South Korea.</h4>
-            <p>Taught alongside multiple Korean teachers. Brainstormed and planned lessons in conjunction with the Korean teachers, including normal daily English lessons and multi-week summer camps.</p>
-
-            <h3>
-              <span className="avoid-wrap">Web Design and Bug Tracking</span>
-              {' '}<span className="avoid-wrap">(2008-2009, 2011-2012)</span></h3>
-            <h4>At GreekBill, Inc. and MemberPlanet in Torrance, California.</h4>
-            <p>Redesigned existing website to improve UI/UX, performed QA testing and bug tracking, and wrote functional documentation.</p>
-
-          </div>
-        </main>
+        </div>
         <FinalLink url="/contact" text="Get in touch" />
       </section>
     );
