bce07291e32a18e51aab73fc9303c855951dbfb0
James Nuanez <james@jamesnuanez.com>
2019-11-07 06:11:46 -0800

    Remove home page section about building this website

--- src/Home.js
+++ src/Home.js
@@ -24,25 +24,6 @@ class Home extends Component {
           </p>
         </div>
 
-        <div className="Home-splash">
-          <div className="Home-splash-video">
-            <h2>Building this website</h2>
-            <video controls
-              poster="/images/temp-video.jpg">
-            </video>
-          </div>
-          <div className="Home-splash-stack">
-            <Stack
-              technologies={['React', 'Node', 'Express', 'MySQL', 'GitHub']}
-            />
-            <div>
-              <a className="button" href="https://github.com/jamesnuanez/jamesnuanez-com-frontend">
-                View code on GitHub
-              </a>
-            </div>
-          </div>
-        </div>
-
         <div className="Home-work">
           <h2>Some other stuff I've worked with</h2>
           <Stack
