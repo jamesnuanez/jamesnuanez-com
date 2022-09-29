156306b01856c13efba70a77b515e951545ceab0
James Nuanez <james@jamesnuanez.com>
2022-09-28 23:16:24 -0700

    Replace curly quote with straight quote

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
           </Subtitle>
           <P>
             From design to deployment, I create fluid and intuitive user
-            interfaces that are fast, responsive, and accessible, and when I’m
+            interfaces that are fast, responsive, and accessible, and when I'm
             not making things from scratch, I clean up codebases, relentlessly
             removing the cruft and finding ways to make the code easier to
             understand, safer to deploy, and more performant.
